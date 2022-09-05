e4c9e4771bf8a3a73ddd84d36c2e2b4ec7b0e81f
Luck <1071626267@qq.com>
2022-09-05 22:41:23 +0800

    第一次提交

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,99 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <a-input placeholder="请输入任务" class="my_ipt" :value="inputValue" @change="valueChange" />
+    <a-button type="primary" @click="addItemToList">添加事项</a-button>
+    <a-list bordered :dataSource="list" class="dt_list">
+      <a-list-item slot="renderItem" slot-scope="item">
+        <!-- 复选框 -->
+        <a-checkbox
+          :checked="item.done"
+          @change="
+            e => {
+              changeChecked(e, item.id)
+            }
+          "
+          >{{ item.info }}</a-checkbox
+        >
+        <!-- 删除链接 -->
+        <a slot="actions" @click="removeItemById(item.id)">删除</a>
+      </a-list-item>
+
+      <!-- footer区域 -->
+      <div class="footer" slot="footer">
+        <span>{{ unDoneListLength }}条剩余</span>
+        <a-button-group>
+          <a-button type="highLightBtn == all? primary : default" @click="changeList('all')">全部</a-button>
+          <a-button type="highLightBtn == undone? primary : default" @click="changeList('undone')">未完成</a-button>
+          <a-button type="highLightBtn == done? primary : default" @click="changeList('done')">已完成</a-button>
+        </a-button-group>
+        <a @click="clean">清除已完成</a>
+      </div>
+    </a-list>
   </div>
 </template>
-
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import { mapState, mapMutations, mapGetters } from 'vuex'
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  name: 'app',
+  data () {
+    return {}
+  },
+  computed: {
+    ...mapState(['list', 'inputValue', 'highLightBtn']),
+    ...mapGetters(['unDoneListLength'])
+  },
+  created () {
+    this.$store.dispatch('getList')
+  },
+  methods: {
+    ...mapMutations(['initInputValue', 'addItem', 'removeItem', 'changeStatus', 'cleanDone', 'changeHighLight']),
+    valueChange (e) {
+      // console.log(e.target.value)
+      this.initInputValue(e.target.value)
+    },
+    addItemToList () {
+      if (this.inputValue.trim().length <= 0) {
+        this.$message.warning('你还没有输入')
+      }
+      this.addItem()
+    },
+    removeItemById (id) {
+      // console.log(id)
+      this.removeItem(id)
+    },
+    changeChecked (e, id) {
+      // console.log(e.target.checked, id)
+      const params = {
+        id: id,
+        status: e.target.checked
+      }
+      this.changeStatus(params)
+    },
+    clean () {
+      this.cleanDone()
+    },
+    changeList (btnName) {
+      // console.log(btnName)
+      this.changeHighLight(btnName)
+    }
   }
 }
 </script>
-
-<style lang="less">
+<style scoped>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  padding: 10px;
+}
+.my_ipt {
+  width: 500px;
+  margin-right: 10px;
+}
+.dt_list {
+  width: 500px;
+  margin-top: 10px;
+}
+.footer {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
 }
 </style>
--- src/store/index.js
+++ src/store/index.js
@@ -1,16 +1,63 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import axios from 'axios'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    list: [],
+    inputValue: 'aaa',
+    highLightBtn: 'all'
   },
   getters: {
+    unDoneListLength (state) {
+      return state.list.filter(item => item.done === false).length
+    }
   },
   mutations: {
+    initList (state, list) {
+      state.list = list
+    },
+    initInputValue (state, value) {
+      state.inputValue = value
+    },
+    addItem (state) {
+      const obj = {
+        id: state.list.length,
+        info: state.inputValue,
+        done: false
+      }
+      state.list.push(obj)
+      state.inputValue = ''
+    },
+    removeItem (state, id) {
+      const res = state.list.filter(item => {
+        return item.id !== id
+      })
+      state.list = res
+    },
+    changeStatus (state, params) {
+      const i = state.list.findIndex(item => item.id === params.id)
+      if (i !== -1) {
+        state.list[params.id].done = params.status
+      }
+      // console.log(params, state.list[params.id])
+    },
+    cleanDone (state) {
+      state.list = state.list.filter(item => item.done === false)
+    },
+    changeHighLight (state, btnName) {
+      state.highLightBtn = btnName
+      console.log(btnName)
+    }
   },
   actions: {
+    async getList (context) {
+      const { data: res } = await axios.get('/listData.json')
+      // console.log(res)
+      context.commit('initList', res)
+    }
   },
   modules: {
   }
